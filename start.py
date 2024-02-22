086f88d6b78a4e1f2fd2f4fa26f522db9e08a2b3
Clay Coppage <clay@horizon3.ai>
2024-02-22 00:43:46 -0500

    use proper directory

--- start.py
+++ start.py
@@ -44,17 +44,22 @@ def make_op_path(base_path: str) -> UUID:
 
     return op_name
 
-def copy_specific_modules(src_dir, dest_dir, modules_to_copy):
+
+def copy_specific_modules(
+    script_path: str, dest_dir: str, modules_to_copy: list
+) -> None:
+    src_dir = os.path.join(script_path, "modules")
     for module in modules_to_copy:
         src_folder_path = os.path.join(src_dir, module)
         dest_folder_path = os.path.join(dest_dir, module)
 
-        # Copy the entire folder to the destination directory
         shutil.copytree(src_folder_path, dest_folder_path)
 
+
 if __name__ == "__main__":
-    creds_file = "keys.yaml"
-    providers = "providers.yaml"
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    creds_file = f"{script_path}/keys.yaml"
+    providers = f"{script_path}/providers.yaml"
     base_path = "/tmp"
 
     creds = read_creds_file(creds_file)
@@ -65,14 +70,6 @@ if __name__ == "__main__":
     logger.info(valid_providers)
     chain_id = make_op_path(base_path)
 
-    src_directory = os.getcwd()
-
-    # Set the destination directory
     dest_directory = f"{base_path}/{chain_id}"
-
-    # Specify the folders to copy
     folders_to_copy = valid_providers
-
-    # Call the function to copy specific folders
-    copy_specific_modules(os.path.join(src_directory, "modules"), dest_directory, folders_to_copy)
-
+    copy_specific_modules(script_path, dest_directory, folders_to_copy)
