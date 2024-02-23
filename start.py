f1c39a98a2a89ec320193b44edadae5355cc2ac8
Clay Coppage <clay@horizon3.ai>
2024-02-23 13:15:47 -0500

    working to create custom module names and tf file

--- start.py
+++ start.py
@@ -1,12 +1,15 @@
 import os
+import random
 import shutil
 import subprocess
 from pathlib import Path
 from uuid import UUID, uuid4
 
 import yaml
+from jinja2 import Environment, FileSystemLoader
 from loguru import logger
 
+environment = Environment(loader=FileSystemLoader(f"{os.path.dirname(os.path.abspath(__file__))}/templates/"))
 
 def read_creds_file(creds) -> dict:
     with open(creds, "r") as creds_file:
@@ -15,7 +18,7 @@ def read_creds_file(creds) -> dict:
     return all_creds
 
 
-def gather_providers(providers) -> list:
+def gather_providers(providers) -> list[str]:
     with open(providers, "r") as file:
         all_providers = yaml.safe_load(file)
 
@@ -39,21 +42,52 @@ def make_op_path(base_path: str) -> UUID:
     logger.info(f"Creating path: {directory_path}")
     directory_path.mkdir()
 
-    logger.info(f"Deleting path: {directory_path}")
-    directory_path.rmdir()
+    # logger.info(f"Deleting path: {directory_path}")
+    # directory_path.rmdir()
 
     return op_name
+
+
+def choose_two(providers: list) -> list[str]:
+    chosen_providers = random.choices(providers, k=2)
+
+    return chosen_providers
 
 
 def copy_specific_modules(
     script_path: str, dest_dir: str, modules_to_copy: list
 ) -> None:
     src_dir = os.path.join(script_path, "modules")
-    for module in modules_to_copy:
+    for index, module in enumerate(modules_to_copy):
+        dest_folder_name = f"hop{index}" + "_" + str(module)
         src_folder_path = os.path.join(src_dir, module)
-        dest_folder_path = os.path.join(dest_dir, module)
+        dest_folder_path = os.path.join(dest_dir, dest_folder_name)
 
         shutil.copytree(src_folder_path, dest_folder_path)
+
+
+def setup_terraform(
+    script_path: str, creds_file: str, providers: str, base_path: str
+) -> None:
+    creds = read_creds_file(creds_file)
+    providers = gather_providers(providers)
+
+    valid_providers = providers_with_creds(creds, list(providers))
+    logger.success(f"Credentials discovered for {valid_providers}")
+
+    select_two = choose_two(valid_providers)
+    logger.success(f"Providers chosen: {select_two}")
+
+    chain_id = make_op_path(base_path)
+    dest_directory = f"{base_path}/{chain_id}/modules"
+
+    copy_specific_modules(script_path, dest_directory, select_two)
+    for index, module in enumerate(select_two):
+        provider = f"hop{index}" + "_" + str(module)
+        template = environment.get_template("main.tf.j2")
+
+        with open(filename, mode="w", encoding="utf-8") as message:
+        message.write(content)
 
 
 if __name__ == "__main__":
@@ -62,11 +96,4 @@ if __name__ == "__main__":
     providers = f"{script_path}/providers.yaml"
     base_path = "/tmp"
 
-    creds = read_creds_file(creds_file)
-    providers = gather_providers(providers)
-    valid_providers = providers_with_creds(creds, providers)
-    logger.success(f"Credentials discovered for {valid_providers}")
-    chain_id = make_op_path(base_path)
-    dest_directory = f"{base_path}/{chain_id}"
-
-    copy_specific_modules(script_path, dest_directory, valid_providers)
+    setup_terraform(script_path, creds_file, providers, base_path)
