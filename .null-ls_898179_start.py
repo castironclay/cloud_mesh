baab6665a793c56496c54b80f08bfec797b32528
Clay Coppage <clycopp@gmail.com>
2024-04-14 12:44:54 -0400

    no proxy, rename role

--- .null-ls_898179_start.py
+++ .null-ls_898179_start.py
@@ -5,7 +5,7 @@ from python_on_whales import docker
 script_path = os.path.dirname(os.path.abspath(__file__))
 docker.run(
     "cloud_mesh:latest",
-    ["/bin/bash"],
+    ["/work/cloud_mesh/build.py"],
     volumes=[
         (script_path, "/work/cloud_mesh"),
         ("/tmp/", "/tmp/"),
