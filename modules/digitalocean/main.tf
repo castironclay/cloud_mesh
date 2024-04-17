527b1d81dd128446b01c3b37be7938a6d2914a9d
Clay Coppage <clay@horizon3.ai>
2024-04-16 21:22:36 -0400

    add digital ocean regions

--- modules/digitalocean/main.tf
+++ modules/digitalocean/main.tf
@@ -1,22 +1,9 @@
-data "digitalocean_regions" "available" {
-  filter {
-    key    = "available"
-    values = ["true"]
-  }
-}
-
-resource "random_integer" "region_number" {
-  depends_on = [data.digitalocean_regions.available]
-  min        = 1
-  max        = length(data.digitalocean_regions.available.regions)
-}
-
 resource "digitalocean_droplet" "droplet" {
   depends_on = [digitalocean_ssh_key.key]
   name       = var.name
   size       = "s-1vcpu-1gb"
   image      = "debian-12-x64"
-  region     = element(data.digitalocean_regions.available.regions, random_integer.region_number.result).slug
+  region     = random_shuffle.regions.result[0]
   ssh_keys   = [digitalocean_ssh_key.key.fingerprint]
 
   provisioner "remote-exec" {
@@ -36,3 +23,21 @@ resource "digitalocean_ssh_key" "key" {
   public_key = file(var.public_keyname)
 
 }
+
+resource "random_shuffle" "regions" {
+  input = [
+    "nyc1", # NYC1 - New York City, United States
+    "nyc3", # NYC3 - New York City, United States
+    "ams3", # AMS3 - Amsterdam, the Netherlands
+    "sfo2", # SFO2 - San Francisco, United States
+    "sfo3", # SFO3 - San Francisco, United States
+    "sgp1", # SGP1 - Singapore
+    "lon1", # LON1 - London, United Kingdom
+    "fra1", # FRA1 - Frankfurt, Germany
+    "tor1", # TOR1 - Toronto, Canada
+    "blr1", # BLR1 - Bangalore, India
+    "syd1", # SYD1 - Sydney, Australia
+  ]
+  result_count = 1
+}
+
