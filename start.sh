2a4983d53a2580af3258b44e871338644dd05935
root <dev@vm>
2024-05-04 14:16:30 -0400

    Add dns to docker run command

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run --rm -it --name sr --sysctl net.ipv6.conf.all.disable_ipv6=0 --privileged --cap-add net_admin -v /lib/modules:/lib/modules sr:latest
+docker run --rm -it --name sr --sysctl net.ipv6.conf.all.disable_ipv6=0 --dns=1.1.1.1 --privileged --cap-add net_admin -v /lib/modules:/lib/modules sr:latest
