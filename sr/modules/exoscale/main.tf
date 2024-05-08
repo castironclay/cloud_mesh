6eb67c2c124a00ebb27be1d7fa6e753fd7895a09
Clay Coppage <clay@Clays-MacBook-Pro.local>
2024-05-07 21:35:28 -0400

    add security group to exoscale

--- sr/modules/exoscale/main.tf
+++ sr/modules/exoscale/main.tf
@@ -4,13 +4,14 @@ data "exoscale_template" "my_template" {
 }
 
 resource "exoscale_compute_instance" "my_instance" {
-  depends_on  = [data.exoscale_template.my_template, exoscale_ssh_key.my_ssh_key]
-  zone        = random_shuffle.regions.result[0]
-  name        = var.name
-  template_id = data.exoscale_template.my_template.id
-  type        = "standard.medium"
-  disk_size   = 10
-  ssh_key     = exoscale_ssh_key.my_ssh_key.name
+  depends_on         = [data.exoscale_template.my_template, exoscale_ssh_key.my_ssh_key]
+  zone               = random_shuffle.regions.result[0]
+  name               = var.name
+  template_id        = data.exoscale_template.my_template.id
+  type               = "standard.medium"
+  disk_size          = 10
+  ssh_key            = exoscale_ssh_key.my_ssh_key.name
+  security_group_ids = [exoscale_security_group.security_group.id]
   provisioner "remote-exec" {
     inline = ["echo 'Im alive!'"]
 
@@ -21,7 +22,28 @@ resource "exoscale_compute_instance" "my_instance" {
       host        = exoscale_compute_instance.my_instance.public_ip_address
     }
   }
+}
 
+resource "exoscale_security_group" "security_group" {
+  name = var.name
+}
+
+resource "exoscale_security_group_rule" "security_group_rule_tcp" {
+  security_group_id = exoscale_security_group.security_group.id
+  type              = "INGRESS"
+  protocol          = "TCP"
+  cidr              = "0.0.0.0/0"
+  start_port        = 1
+  end_port          = 65535
+}
+
+resource "exoscale_security_group_rule" "security_group_rule_udp" {
+  security_group_id = exoscale_security_group.security_group.id
+  type              = "INGRESS"
+  protocol          = "UDP"
+  cidr              = "0.0.0.0/0"
+  start_port        = 1
+  end_port          = 65535
 }
 
 resource "exoscale_ssh_key" "my_ssh_key" {
